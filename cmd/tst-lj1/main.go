a35b7de44bb81fa0879806b15378a37c79a57a5c
urso <steffen.siering@elastic.co>
2016-05-11 16:51:57 +0200

    Add tst-lj1 and tst-lj2 for receiving from different protocol versions

---
+++ cmd/tst-lj1/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/urso/go-lumber/v1/server"
+)
+
+func main() {
+	s, err := server.ListenAndServe(":5044")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("tcp server up")
+
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, os.Interrupt)
+	go func() {
+		<-sig
+		_ = s.Close()
+	}()
+
+	for batch := range s.ReceiveChan() {
+		log.Printf("Received batch of %v events\n", len(batch.Events))
+		batch.ACK()
+	}
+}
